b2d880b7d158e7bd66af8c2c7026af0ab357b676
Mahib22 <mahib.arib@gmail.com>
2023-03-29 22:20:19 +0700

    install tailwind, flowbite, vue-router

---
+++ src/components/NavbarApp.vue
@@ -0,0 +1,74 @@
+<script setup>
+import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink } from "flowbite-vue";
+</script>
+
+<template>
+  <header class="text-gray-600 body-font">
+    <div
+      class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+    >
+      <a
+        class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
+          ></path>
+        </svg>
+        <span class="ml-3 text-xl">Tailblocks</span>
+      </a>
+      <nav
+        class="md:ml-auto flex flex-wrap items-center text-base justify-center"
+      >
+        <a class="mr-5 hover:text-gray-900">First Link</a>
+        <a class="mr-5 hover:text-gray-900">Second Link</a>
+        <a class="mr-5 hover:text-gray-900">Third Link</a>
+        <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+      </nav>
+    </div>
+  </header>
+
+  <Navbar>
+    <template #logo>
+      <NavbarLogo
+        link="https://www.google.com/"
+        alt="Flowbite logo"
+        image-url="https://flowbite.com/docs/images/logo.svg"
+      >
+        Flowbite
+      </NavbarLogo>
+    </template>
+    <template #default="{ isShowMenu }">
+      <NavbarCollapse :isShowMenu="isShowMenu">
+        <NavbarLink is-active>Home</NavbarLink>
+        <NavbarLink>Accomodations</NavbarLink>
+        <NavbarLink>Gallery</NavbarLink>
+        <NavbarLink>News</NavbarLink>
+        <NavbarLink>Contact</NavbarLink>
+      </NavbarCollapse>
+    </template>
+    <template #right-side>
+      <button
+        type="button"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Get started
+      </button>
+    </template>
+  </Navbar>
+</template>
+
+<script>
+export default {
+  name: "NavbarApp",
+};
+</script>
