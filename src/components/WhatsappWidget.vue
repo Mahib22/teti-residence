78cb7fb6622ef4ed0349f20c65a4c8e70074d456
Mahib22 <mahib.arib@gmail.com>
2023-10-14 16:35:45 +0700

    sudah diupdate

--- src/components/WhatsappWidget.vue
+++ src/components/WhatsappWidget.vue
@@ -3,7 +3,19 @@
     aria-label="Chat on WhatsApp"
     href="https://wa.me/6281315013050"
     target="_blank"
-    class="fixed z-50 bottom-5 right-5 2xl:bottom-10 2xl:right-10"
+    class="fixed hidden md:block z-50 bottom-5 right-5 2xl:bottom-10 2xl:right-10"
+  >
+    <img
+      alt="Chat on WhatsApp"
+      src="../../public/img/icon/WhatsAppButtonGreenLarge.svg"
+    />
+  </a>
+
+  <a
+    aria-label="Chat on WhatsApp"
+    href="https://wa.me/6281315013050"
+    target="_blank"
+    class="fixed block md:hidden z-50 left-3 bottom-20 mb-1"
   >
     <img
       alt="Chat on WhatsApp"
